1ba61da0e55b234b6656ad32a66befcf37c2679e
Ben Lynch <ben.lynch@eroad.com>
2015-10-20 11:35:07 +1300

    tests

---
+++ test/example.js
@@ -0,0 +1,45 @@
+import webdriverio from 'webdriverio';
+import chai from 'chai';
+
+let options = {
+	desiredCapabilities: {
+		browserName: 'chrome'
+	}
+};
+
+let assert = chai.assert,
+		expect = chai.expect,
+		client = webdriverio.remote(options);
+
+
+describe('Load Page', function() {
+
+	this.timeout(99999999);
+
+	before((done) => {
+		client.init(done);
+	});
+
+	it('Title Should be Codeship', (done) => {
+		client
+      .url("http://localhost:3000")
+			.getTitle().then((title) => {
+				expect(title).to.equal("codeship")
+			})
+			.call(done);
+	});
+
+	it('Should have text Foo', (done) => {
+		client
+			.getText("#main", (err, text) => {
+        // expect(err)to.be.undefined;
+        expect(text).to.equal("Foo")
+      })
+			.call(done);
+	});
+
+	after((done) => {
+		client.end(done);
+	});
+
+});
